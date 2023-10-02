2b640346e1369c973d6bf7ba23d43756098fd5ec
SaoodCS <saood-aslam@outlook.com>
2023-10-02 10:31:23 +0100

    update delete user req body

--- functions/src/deleteUser/reqBodyClass/DeleteUserReqBody.ts
+++ functions/src/deleteUser/reqBodyClass/DeleteUserReqBody.ts
@@ -1,17 +1,12 @@
 export interface IDeleteUserReqBody {
    email: string;
-   password: string;
 }
 
 export default class DeleteUserReqBody {
    static isValid(body: unknown): body is IDeleteUserReqBody {
       if (typeof body !== 'object' || body === null) return false;
-
-      const { email, password } = body as IDeleteUserReqBody;
-
+      const { email } = body as IDeleteUserReqBody;
       if (typeof email !== 'string') return false;
-      if (typeof password !== 'string') return false;
-
       return true;
    }
 }
